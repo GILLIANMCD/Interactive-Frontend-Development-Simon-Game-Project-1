15cd4d53a3e774b43c9da2c102d5d675a31aa767
GILLIANMCD <51652977+GILLIANMCD@users.noreply.github.com>
2019-07-31 09:21:06 +0100

    html css changes js added 1st test to console log

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,52 @@
+let sounds = [
+	new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+	new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+	new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+	new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+];
+
+
+let strict = false;
+let noise = true;
+let start;
+
+const level1 = document.getElementById("level1");
+const level2 = document.getElementById("level2");
+const level3 = document.getElementById("level3");
+const level4 = document.getElementById("level4");
+const turnCounter = document.getElementById("turn");
+const green = document.getElementsByClassName("green");
+const red = document.getElementsByClassName("red");
+const yellow = document.getElementsByClassName("yellow");
+const blue = document.getElementsByClassName("blue");
+const strictButton = document.getElementById("strict");
+const startButton = document.getElementById("start");
+
+
+level1.addEventListener('click', (event) => {
+	console.log("checked")
+})
+
+level2.addEventListener('click', (event) => {
+	console.log("checked")
+})
+
+level3.addEventListener('click', (event) => {
+	console.log("checked")
+})
+
+level4.addEventListener('click', (event) => {
+	console.log("checked")
+})
+
+
+strictButton.addEventListener('click', (event) => {
+	console.log("checked")
+})
+
+startButton.addEventListener('click', (event) => {
+	console.log("checked")
+})
+
+
 
